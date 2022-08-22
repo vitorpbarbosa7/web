e6ba2654e10395b652f2f08465b914124da06fae
Vitor Pereira Barbosa <vitorpbarbosa7@gmail.com>
2022-08-21 21:28:50 -0300

    parent container

--- javascript/miniprojects/javascript-basic-projects/01-color-flipper/setup/hex.js
+++ javascript/miniprojects/javascript-basic-projects/01-color-flipper/setup/hex.js
@@ -1,25 +1,20 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById('btn');
-const color = document.querySelector('.color');
 
 btn.addEventListener('click', function() {
     const hexColor = generateRandomColor();
-    //debug
     console.log(hexColor);
-
-    //action
     document.body.style.backgroundColor = hexColor;
-    color.textContent = hexColor;
     }
 );
 
 function generateRandomColor(){
     HEXLENGTH = 6
-    localColor = '#'
+    color = '#'
     for (let i = 0; i < HEXLENGTH; i++){
-        localColor += getRandomElement()
+        color += getRandomElement()
     }
-    return localColor
+    return color
 }
 
 function getRandomElement(){
