bc26327cc0515dd943bf90d5687232ddbed468ce
vpbmac <vitorpbarbosa7@gmail.com>
2022-09-13 21:27:21 -0300

    array of strings for storing last count values

--- javascript/applied/index.js
+++ javascript/applied/index.js
@@ -1,25 +1,30 @@
 // representation model for the actual html object (DOM)
+let welcomeEl = document.getElementById("welcome-el")
+let user = "Messi"
+let welcomeMessage = `Welcome ${user}`
+welcomeEl.innerText = welcomeMessage 
+welcomeEl.innerText += "👋"
+
 let people = document.getElementById("count-el");
-console.log(people)
+let savedParagraph = document.getElementById("saved-values")
 
 let count = 0;
-
 function increment(){
     count += 1
     console.log(count)
     people.innerText=count
 }
 
+const savedValues = [0];
+let countSave = 0;
 function save() {
-    console.log(count)
+    countSave +=1
+    savedValues[countSave] = count.toString()
+    thirdLast = savedValues.slice(-3)[0]
+    secondLast = savedValues.slice(-2)[0]
+    last = savedValues.slice(-1)[0]
+
+    savedParagraph.innerText = `${last} - ${secondLast} - ${thirdLast}`
 }
 
-save()
 
-
-let welcomeEl = document.getElementById("welcome-el")
-
-let user = "Messi"
-let welcomeMessage = `Welcome ${user}`
-
-welcomeEl.innerText = welcomeMessage
