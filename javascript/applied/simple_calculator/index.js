ea2ee0a3207f76bc46c701bc512622cefdf1c1c7
vpbmac <vitorpbarbosa7@gmail.com>
2022-09-19 21:46:05 -0300

    calculations added to simples calculator

--- javascript/applied/simple_calculator/index.js
+++ javascript/applied/simple_calculator/index.js
@@ -1,1 +1,47 @@
-let 
+let firstNumber = parseFloat(document.getElementById('first-number').value)
+let secondNumber = parseFloat(document.getElementById('second-number').value)
+
+let btnAdd = document.getElementById('add')
+let btnSub = document.getElementById('sub')
+let btnMul = document.getElementById('mul')
+let btnDiv = document.getElementById('div')
+
+btnAdd.addEventListener("click", function() {
+    update(add)
+})
+
+btnSub.addEventListener("click", function() {
+    update(sub)
+})
+
+btnMul.addEventListener("click", function() {
+    update(mul)
+})
+
+btnDiv.addEventListener("click", function(){
+    update(div)
+})
+
+function update(operation){
+    document.getElementById('result-text').textContent = operation()
+    console.log(operation())
+}
+
+function add(){
+    return firstNumber+secondNumber
+}
+
+function sub(){
+    return firstNumber-secondNumber
+}
+
+function mul(){
+    return firstNumber*secondNumber
+}
+
+function div(){
+    return firstNumber/secondNumber
+}
+
+
+
